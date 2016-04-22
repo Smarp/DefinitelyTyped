246a33d347cfd7034bef4be02abbb40dae332ebb
zhenshi <zhenshi@admins-MacBook-Air.local>
2016-04-22 13:42:57 +0300

    Karma-fixture

---
+++ karma-fixture/karma-fixture.ts
@@ -0,0 +1,13 @@
+// Type definitions for Karma-fixture 1.0.6
+// Project: https://github.com/billtrik/karma-fixture
+// Definitions by: Zhen Shi <https://github.com/zoeszhen>
+// Definitions: https://github.com/Smarp/DefinitelyTyped
+
+interface Fixture {
+	el(): void;
+	load(files: any): any;
+	set(html_strings: string): any;
+	cleanup(): void;
+	setBase(fixtureBasePath: any): void;
+}
+declare var fixture: Fixture;
